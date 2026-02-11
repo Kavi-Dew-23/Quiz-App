7f0407eeed5a09d15fbbc96de54d7854a4f65716
Kavi-Dew-23 <dewapuraka@gmail.com>
2026-02-11 20:18:26 +0530

    Stores the highScores in the local storage

--- end.js
+++ end.js
@@ -9,6 +9,20 @@ const mostRecentScore = localStorage.getItem("mostRecentScore");
 
 //set the fianlScore equals to the mostRecentScore
 const finalScore = document.getElementById("finalScore");
+
+// to store in the local storage it should be a empty string
+//localStorage.setItem("highScores", []); //save the highscores as empty string
+
+//converth the above into json string
+//localStorage.setItem("highScores", JSON.stringify([]));
+
+//console.log(localStorage.getItem("highScores")); 
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+console.log(highScores);
+
+const MAX_HIGH_SCORES = 5;
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -23,4 +37,20 @@ saveHighScore = (e) => {
     console.log("Clicked the Save button!");
     e.preventDefault(); // prevent the form taking its default action which is to post to a different page
 
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+    highScores.push(score);
+    console.log(highScores);
+    //console.log(score);
+
+    highScores.sort( (a,b) => b.score - a.score );
+        //if b's score is greater than a score put b's score first than a
+        //return b.score - a.score;
+    //get only the first 5 highscores
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("/");
 }
